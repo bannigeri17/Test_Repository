b948e5b3c16ee285ff42d2e7fa8f6424bf4c142e
bannigeri17 <bannigeri17@gmail.com>
2016-06-27 18:49:29 -0700

    Add Netbeans project to test Bash

---
+++ Game/src/Game/Character.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Game;
+
+/**
+ *
+ * @author bhargav
+ */
+public class Character {
+    private int type;
+    private boolean gucci;
+    private String form;
+    
+    public Character(int t, boolean isGucci){
+        type = t;
+        gucci = isGucci;
+        if (t == 0){
+            form = "average";
+        }
+        if (t == 1){
+            form = "nerd";
+        }
+        if (t == 2){
+            form = "hero";
+        }
+        if (t == 3){
+            form = "villain";
+        }
+        if (t == 4){
+            form = "wizard";
+        }
+        if (t == 5){
+            form = "conman";
+        }
+        if (t == 6){
+            
+        }
+    }
+    
+}
